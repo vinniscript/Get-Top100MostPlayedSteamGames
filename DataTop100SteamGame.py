f4ba2cc432030a6f3de42347c6e33b220547f3af
Vinicius Sousa <viniciusps.1190@gmail.com>
2025-03-19 12:31:15 -0300

    Adição de README

--- DataTop100SteamGame.py
+++ DataTop100SteamGame.py
@@ -1,4 +1,30 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
+
+# Carrega variáveis de ambiente
+load_dotenv('.env')
+
+# Carrega variáveis de ambiente
+STEAM_API_KEY = os.getenv('STEAM_API_KEY')
+if (STEAM_API_KEY is None):
+    raise ValueError('STEAM_API_KEY não configurada')
+else:
+    print('STEAM_API_KEY carregada com sucesso.')
 
 url = "https://api.steampowered.com/ISteamChartsService/GetMostPlayedGames/v1/"
+params = {
+    'key': STEAM_API_KEY
+}
+
+# Faz a requisição
+response = requests.get(url, params=params)
+
+# Verifica o status da requisição
+if response.status_code == 200:
+    data = response.json()
+    print(data) # Exibe a resposta para depuração
+else:
+    print(f"Erro na requisição: {response.status_code}")
+    print(response.text)
---
+++ README.md
@@ -0,0 +1,32 @@
+# Projeto Steam API
+
+## Configuração
+
+1. Crie um '.env' na raiz do projeto com as seguintes variáveis de ambiente:
+
+    ```env
+    STEAM_API_KEYSTEAM=sua_chave_aqui
+    ```
+2. Instale as dependências do projeto:
+    ```bash
+   pip install -r requirements.txt
+    ```
+3. Execute o script 'DataTop100SteamGames.py':
+    ```bash
+    python DataTop100SteamGames.py
+    ```
+Ele criará o arquivo .json com todas as informações dos jogos que será necessário para você trabalhar em cima.
+
+## Estrutura do Projeto
+
+- steamAPI
+    - DataTop100SteamGames.py
+    - README.md
+    - requirements.txt
+    - .env
+
+
+## Desenvolvedores
+
+### Vinicius Pereira de Sousa - RA 12523163043
+### Guilherme 
